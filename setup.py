5c835c885bc0dbdbf8899302cef5354c9c300476
Patrick Uiterwijk <puiterwijk@redhat.com>
2014-01-26 13:55:36 +0100

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ else:
 
 setup(
     name='Flask-OpenID',
-    version='1.2',
+    version='1.2.1',
     url='http://github.com/mitsuhiko/flask-openid/',
     license='BSD',
     author='Armin Ronacher, Patrick Uiterwijk',
